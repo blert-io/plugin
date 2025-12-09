d5c95f94d22724042bd5641e0ff23507a5b9bb29
Alexei Frolov <alexeifrolov3@gmail.com>
2025-12-08 19:17:40 -0800

    Correct cleanup on plugin shut down
    The active challenge was not being deregistered from the event bus,
    keeping a stale instance around.

--- src/main/java/io/blert/BlertPlugin.java
+++ src/main/java/io/blert/BlertPlugin.java
@@ -149,7 +149,9 @@ public class BlertPlugin extends Plugin {
         sidePanel = null;
 
         if (activeChallenge != null) {
+            eventBus.unregister(activeChallenge);
             activeChallenge.terminate();
+            activeChallenge = null;
         }
 
         challenges.clear();
