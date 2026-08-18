cce4d0189697d02fa67625315708b89ee6c4e3d0
Alexei Frolov <alexeifrolov3@gmail.com>
2026-08-17 17:47:29 -0700

    Correctly initialize HP of resurrected Inferno mobs (#59)
    A non-bloblet NPC spawning later than the start of the wave can only be
    a mager resurrection, which spawns with half of its full HP.

--- src/main/java/io/blert/challenges/inferno/InfernoNpc.java
+++ src/main/java/io/blert/challenges/inferno/InfernoNpc.java
@@ -23,6 +23,7 @@
 
 package io.blert.challenges.inferno;
 
+import io.blert.core.Hitpoints;
 import io.blert.core.NpcAttack;
 import java.util.Optional;
 import lombok.Getter;
@@ -103,6 +104,23 @@ public enum InfernoNpc {
 
     boolean isPillar() {
         return this == ROCKY_SUPPORT;
+    }
+
+    boolean isResurrectable() {
+        switch (this) {
+            case BAT:
+            case BLOB:
+            case MELEER:
+            case RANGER:
+            case MAGER:
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    Hitpoints resurrectedHitpoints() {
+        return new Hitpoints((hitpoints + 1) / 2, hitpoints);
     }
 
     @SafeVarargs
